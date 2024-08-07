c82e08ae907bb90ab9fac191df2d924552e776bb
Kleberson dos Santos Mendes Pinto <klebersonsantosmp@gmail.com>
2024-08-07 13:16:07 -0300

    d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import style from "/app/style.css"
 
 export default function Home() {
   return (
   <>
-  <button><Link href={"/about"}>Nada aqui</Link></button>
+  <button className="botao1"><Link href={"/about"}>Nada aqui</Link></button>
   </>
   );
 }
