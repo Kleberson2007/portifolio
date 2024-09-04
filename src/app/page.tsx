56b9534f65c6c4f1f4a56002a000df54380a386d
Kleberson dos Santos Mendes Pinto <klebersonsantosmp@gmail.com>
2024-09-04 17:32:58 -0300

    k

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 'use client'
-import Link from "next/link";
 import "./globals.css";
-import s from "./page.module.css"
 
 export default function Home() {
   return (
-    <div className={s.page}>
-      <div className={s.tittle}>
+    <div className="page">
+      <div className="tittle">
         <h1>meus projetos</h1>
       </div>
-        <a href={"/lista"}>lista de tarefas</a>
-        <a href={"/contador"}>Contador</a>
+      <div id="projetos">
+        <a id="lista" href={"/lista"}>lista de tarefas</a>
+        <a id="contador" href={"/contador"}>Contador</a>
       </div>
+    </div>
   );
 }
